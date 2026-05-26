d8cc77def025d109c183cf00481456e8cff0714d
latakshsariyapatidar <latakshpatidar@gmail.com>
2026-05-26 19:24:18 +0530

    Fixed cors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,12 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    origin = 'https://angular-rbac-records-dashboard.vercel.app/',
-    credentials = true,
-    methods = ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders = ['Content-Type', 'Authorization']
-));
+app.use(cors({
+  origin: 'https://angular-rbac-records-dashboard.vercel.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 app.get('/', (req, res) => {
